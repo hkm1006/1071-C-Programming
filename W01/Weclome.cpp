8dc9bf750b28e6b6668a7262caffd0f0ad5da1bb
hkm1006 <jackhkm1006@gmail.com>
2018-09-17 17:57:38 +0800

    Adding W01 files

---
+++ W01/Weclome.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+ printf("Welcome to C Programming\n");
+ printf("My name: ¦ó±Ò©ú\n");
+ return 0;
+}
---
+++ W01/equation .cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a,b,c;
+	double x1,x2;
+	printf("Enter a b c: ");
+	scanf("%d %d %d",&a,&b,&c);
+	if(b*b-4*a*c>=0){
+		x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+		x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+		printf("For equation 2 x^2 -3 x + 1 =0, two roots are\n");
+		printf("x1=%.1f\nx2=%.1f\n",x1,x2);
+	}else{
+		printf("no real roots\n");
+	}
+}
+
+
+/*
+Enter a b c: 2 -3 1
+For equation 2 x^2 -3 x + 1 =0, two roots are
+x1 = 1.0
+x2 = 0.5
+*/
